96b4373864c26c285f086a6bcece5351a66a7ede
gycaa <1261675701@qq.com>
2023-08-01 11:48:44 +0800

    comment to changes

---
+++ src/test/java/com/cy/store/mapper/UserMapperTests.java
@@ -0,0 +1,75 @@
+package com.cy.store.mapper;
+
+import com.cy.store.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+//@SpringBootTest表示当前的类是一个测试类,不会随同项目一块打包
+@SpringBootTest
+
+/**
+ * 1.@RunWith表示启动这个单元测试类,否则这个单元测试类是不能运行的,需要传递
+ * 一个参数,该参数必须是SpringRunner.class即SpringRunner的实例类型
+ * 2.敲完@RunWith(SpringRunner.class)后鼠标分别放在SpringRunner和@RunWith上按alt+enter分别导入包
+ * 3.单元测试类中出现的方法必须是单元测试方法
+ * 4.单元测试方法的特点:必须被@Test注解修饰;返回值类型必须是void;方法的参数列表不指定任何类型;方法的访问修饰符必须是public
+ */
+@RunWith(SpringRunner.class)
+public class UserMapperTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void insert() {
+        User user = new User();
+        user.setUsername("张6");
+        user.setPassword("123456");
+        Integer rows = userMapper.insert(user);
+        System.out.println(rows);
+    }
+
+    @Test
+    public void findByUsername() {
+        User user = userMapper.findByUsername("张三");
+        System.out.println(user);
+    }
+
+    @Test
+    public void updatePasswordByUid(){
+        userMapper.updatePasswordByUid(
+                10,
+                "321",
+                "管理员",
+                new Date());
+    }
+
+    @Test
+    public void findByUid(){
+        System.out.println(userMapper.findByUid(10));
+    }
+
+    @Test
+    public void updateInfoByUid() {
+        User user = new User();
+        user.setUid(11);
+        user.setPhone("13333688");
+        user.setEmail("1454@qq.com");
+        user.setGender(1);
+        userMapper.updateInfoByUid(user);
+    }
+
+    @Test
+    public void updateAvatarByUid() {
+        userMapper.updateAvatarByUid(
+                11,
+                "abc",
+                "mxy",
+                new Date());
+    }
+}
